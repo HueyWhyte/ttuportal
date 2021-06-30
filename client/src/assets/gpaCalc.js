fe71dcf971fe664e2ff7b292e59d518177b66d36
HueyWhyte <stanleykwaminaotabil@gmail.com>
2021-06-30 10:48:33 +0000

    new gpa calculator

--- client/src/assets/gpaCalc.js
+++ client/src/assets/gpaCalc.js
@@ -3,17 +3,32 @@ export const cgpaCalculator = (results) => {
   let totalCreditHours = 0;
 
   results.forEach((result) => {
-    if (result.score >= 80) {
+    if (result.score >= 85 && result.score <= 100) {
+      totalGradePoint += 5.0 * parseInt(result.course.creditHours);
+      totalCreditHours += parseInt(result.course.creditHours);
+    } else if (result.score >= 80 && result.score <= 84) {
+      totalGradePoint += 4.5 * parseInt(result.course.creditHours);
+      totalCreditHours += parseInt(result.course.creditHours);
+    } else if (result.score >= 75 && result.score <= 79) {
       totalGradePoint += 4.0 * parseInt(result.course.creditHours);
       totalCreditHours += parseInt(result.course.creditHours);
-    } else if (result.score >= 69) {
+    } else if (result.score >= 70 && result.score <= 74) {
       totalGradePoint += 3.5 * parseInt(result.course.creditHours);
       totalCreditHours += parseInt(result.course.creditHours);
-    } else if (result.score >= 59) {
+    } else if (result.score >= 65 && result.score <= 69) {
       totalGradePoint += 3.0 * parseInt(result.course.creditHours);
       totalCreditHours += parseInt(result.course.creditHours);
-    } else if (result.score >= 49) {
+    } else if (result.score >= 60 && result.score <= 64) {
       totalGradePoint += 2.5 * parseInt(result.course.creditHours);
+      totalCreditHours += parseInt(result.course.creditHours);
+    } else if (result.score >= 55 && result.score <= 59) {
+      totalGradePoint += 2.0 * parseInt(result.course.creditHours);
+      totalCreditHours += parseInt(result.course.creditHours);
+    } else if (result.score >= 50 && result.score <= 54) {
+      totalGradePoint += 3.5 * parseInt(result.course.creditHours);
+      totalCreditHours += parseInt(result.course.creditHours);
+    } else if (result.score >= 0 && result.score <= 49) {
+      totalGradePoint += 0 * parseInt(result.course.creditHours);
       totalCreditHours += parseInt(result.course.creditHours);
     }
   });
